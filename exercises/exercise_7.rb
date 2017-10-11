7bb894e8f40708b1a6aee4ea8ea79582f5c95919
Peter Hunt <peter.s.hunt@gmail.com>
2017-10-11 17:53:20 +0000

    add validations to store and employee classes

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,24 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+class Store < ActiveRecord::Base
+  has_many :employees
+  validates_length_of :name, :minimum => 3
+  validates_numericality_of :annual_revenue, :greater_than_or_equal_to => 0
+end
+
+class Employee < ActiveRecord::Base
+  belongs_to :store
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :hourly_rate, presence: true, :inclusion => {:in => [40,200]}
+  validates :store, presence: true
+end
+
+
+puts 'Name a store'
+@store_name = gets.chomp
+
+new_store = Store.create!(name: @store_name)
+new_store.errors.full_messages
+
